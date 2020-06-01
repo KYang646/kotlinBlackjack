af11fdff4eb6d627e75f391ae752b85ee62cf551
Kyang646 <kimballyang@pursuit>
2020-06-01 05:58:19 -0400

    beginning

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    fun createDeck() {
+        // create a deck 1 to King with 4 different suits
+        // cannot have duplicate card, ie. 5 of hearts and 5 of hearts
+    }
+
+    fun firstCard(start: Int, end: Int): IntRange {
+        
+    }
+
+
+
+}
