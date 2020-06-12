7001be335e283a384f361b8f216df12053b1e434
Kyang646 <kimballyang@pursuit>
2020-06-11 23:16:20 -0400

    Using starter code from videos

--- src/card.kt
+++ src/card.kt
@@ -1,8 +1,21 @@
 class Card {
 
-    val suits: Array<String> = arrayOf(
-            "♥", "♠", "♦", "♣")
+//    val suits: Array<String> = arrayOf(
+//            "♥", "♠", "♦", "♣")
 
     val someCard: Array<String> = arrayOf("Ace", "2", "3", "4", "5", "6", "7", "8",
                                             "9", "10", "Jack", "Queen", "King")
+}
+
+fun main() {
+    var deck = createDeck()
+    var hand = dealHand(number: 2, deck)
+    var total = evaluateHand(hand)
+    printResults(hand, total)
+}
+
+fun createDeck() : MutableSet<Card> {
+    var suits = listOf("\u2663","\u2660","\u2666","\u2665")
+    
+
 }
